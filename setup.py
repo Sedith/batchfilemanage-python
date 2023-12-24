dcc15b8ed0dd708d683739186da1965fce23e978
Sedith <martin.jacquet@posteo.net>
2023-12-24 19:10:03 +0100

    rename package to batch-file-manage

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='batch-file-manage-python',
+    name='batch-file-manage',
     version='1.0',
     packages=setuptools.find_packages(),
     scripts=[
