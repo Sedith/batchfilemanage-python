c9af5f9a6e2d0f4c438746d7abad3ae4f2c81e38
Sedith <martin.jacquet@posteo.net>
2023-12-25 21:28:40 +0100

    cut script done

--- batchfilemanage/cut.py
+++ batchfilemanage/cut.py
@@ -4,21 +4,25 @@ import cv2
 from os import remove
 from os.path import join
 from batchfilemanage.utils import sorted_aphanumeric, prompt, remove_ext, get_ext
+import matplotlib.pyplot as plt
 
 
 ## command description line
 desc = 'Cut images in half (vertically).'
 
-def create_args(subparsers):
-    parser = subparsers.add_parser('cut', description=desc, help=desc)
+def create_args(subparsers=None):
+    if subparsers:
+        parser = subparsers.add_parser('cut', description=desc, help=desc)
+    else:
+        parser = argparse.ArgumentParser(description=desc)
 
     parser.add_argument('-p', dest='path', help='Path to working directory.', default='./', type=str)
-    parser.add_argument('-s', dest='direction', help='Reading direction (lr: left to write; rl: right to left)', default='rl', type=str, choices=['lr', 'rl'])
-    parser.add_argument('-i', dest='interactive', help='Interactive prompt for each image.', action='store_true')
     parser.add_argument('-d', dest='delete', help='Delete source file after cutting.', action='store_true')
+    parser.add_argument('-i', dest='interactive', help='Interactive prompt.', action='store_true')
+    parser.add_argument('-s', dest='direction', help='Reading direction (lr: left to write; rl: right to left)', default='rl', type=str, choices=['lr', 'rl'])
     parser.add_argument('-n', dest='ignore', help='Filenames of images to ignore.', default=[], type=str, nargs='*')
     parser.add_argument('-o', dest='offset', help='Offset in pixels (positive to the right).', default=0, type=int)
-    parser.add_argument('-r', dest='resize', help='Resize factor for display.', default=0.5, type=float)
+    parser.add_argument('-r', dest='resize', help='Resize factor for display (in interactive mode).', default=0.5, type=float)
 
     return parser
 
@@ -30,54 +34,63 @@ def cut(img, offset):
     return (left, right)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Cut images in half.')
-    parser.add_argument('-p', dest='path', help='Path to working directory.', default='./', type=str)
-    parser.add_argument('-s', dest='direction', help='Reading direction (lr: left to write; rl: right to left)', default='rl', type=str, choices=['lr', 'rl'])
-    parser.add_argument('-i', dest='interactive', help='Display and prompt for each image.', action='store_true')
-    parser.add_argument('-d', dest='delete', help='Delete source file after cutting.', action='store_true')
-    parser.add_argument('-n', dest='ignore', help='Filenames of images to ignore.', default=[], type=str, nargs='*')
-    parser.add_argument('-o', dest='offset', help='Offset in pixels (positive to the right).', default=0, type=int)
-    parser.add_argument('-r', dest='resize', help='Resize factor for display.', default=0.5, type=float)
-    args = parser.parse_args()
+def main(args):
+    ## get list of images
+    images = sorted_aphanumeric(args.path, ignore=args.ignore, ext=['jpg', 'png', 'jpeg'])
 
-    path = args.path
-
-    offset = args.offset
-    for file in sorted_aphanumeric(path, ignore=args.ignore, ext=['jpg', 'png', 'jpeg']):
-        img = cv2.imread(join(path, file))
+    for file in images:
+        img = cv2.imread(join(args.path, file))
+        (l, r) = cut(img, args.offset)
         save = True
-        (l, r) = cut(img, offset)
+
         if args.interactive:
-            stop = False
-            while not stop:
-                cv2.imshow('left', cv2.resize(l, (0, 0), fx=args.resize, fy=args.resize))
-                cv2.imshow('right', cv2.resize(r, (0, 0), fx=args.resize, fy=args.resize))
-                cv2.waitKey(100)
+            fig = None
+
+            while 1:
+                ## check that figure is still open
+                if not fig or not plt.fignum_exists(fig.number):
+                    fig = plt.figure('cropping')
+                fig.clear()
+                ax = [fig.add_subplot(121), fig.add_subplot(122)]
+                ax[0].get_yaxis().set_visible(False)
+                ax[1].get_yaxis().set_visible(False)
+                ax[0].set_xlabel('left image')
+                ax[1].set_xlabel('right image')
+                ax[0].imshow(l)
+                ax[1].imshow(r)
+                plt.show(block=False)
+                
                 ans = prompt('%s - (o)k/(i)gnore/(r)ecut/(a)ll/(c)ancel' % file, ['o', 'i', 'r', 'a', 'c'])
                 if ans == 'o':
-                    stop = True
                     save = True
+                    break
                 elif ans == 'i':
-                    stop = True
                     save = False
+                    break
                 elif ans == 'r':
-                    offset = prompt('new offset value', 0)
-                    (l, r) = cut(img, offset)
+                    args.offset = prompt('new offset value', 0)
+                    (l, r) = cut(img, args.offset)
                 elif ans == 'a':
                     args.interactive = False
-                    stop = True
+                    break
                 elif ans == 'c':
                     exit()
+
         if save:
-            print('Cutting %s with offset of %i' % (file, offset))
+            print('Cutting %s with offset of %i' % (file, args.offset))
             file_noext = remove_ext(file)
             ext = get_ext(file)
             if args.direction == 'rl': # right side then left side
-                cv2.imwrite(join(path, file_noext + '_1.' + ext), r)
-                cv2.imwrite(join(path, file_noext + '_2.' + ext), l)
+                cv2.imwrite(join(args.path, file_noext + '_1.' + ext), r)
+                cv2.imwrite(join(args.path, file_noext + '_2.' + ext), l)
             else:
-                cv2.imwrite(join(path, file_noext + '_2.' + ext), r)
-                cv2.imwrite(join(path, file_noext + '_1.' + ext), l)
+                cv2.imwrite(join(args.path, file_noext + '_2.' + ext), r)
+                cv2.imwrite(join(args.path, file_noext + '_1.' + ext), l)
             if args.delete:
-                remove(join(path, file))
+                remove(join(args.path, file))
+
+
+if __name__ == '__main__':
+    parser = create_args()
+    args = parser.parse_args()
+    main(args)
